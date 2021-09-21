ba0ba00bfec6f4c2e3a486298b8da11b730158bf
Dimitar Spasovski <dimitar.spasovski@movic.nl>
2021-09-21 21:19:54 +0200

    Initial Commit

---
+++ FootballSwiftUI/FootballSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FootballSwiftUIApp.swift
+//  FootballSwiftUI
+//
+//  Created by Dimitar Spasovski on 9/21/21.
+//
+
+import SwiftUI
+
+@main
+struct FootballSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
